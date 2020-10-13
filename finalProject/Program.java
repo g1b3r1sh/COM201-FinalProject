697746a9bec2865e5fc1de60ce7ecd2ffbdb5192
repl.it user <replituser@example.com>
2020-10-13 15:57:31 +0000

    Updated test Graph

--- finalProject/Program.java
+++ finalProject/Program.java
@@ -1,5 +1,6 @@
 package finalProject;
 
+import java.util.ArrayList;
 public class Program {
 	public static void main(String[] args) {
 		Graph foo = new Graph();
@@ -9,19 +10,15 @@ public class Program {
 		 * |
 		 * 1—2
 		 */
+		ArrayList<Integer> bar = new ArrayList<>();
+			bar.add(100);
+			bar.add(101);
+			bar.add(102);
+			bar.add(103);
 		foo.add(0);
 		foo.add(1);
 		foo.add(2);
 		foo.connect(foo.getNode(0), foo.getNode(1), 180, 0, 2, null);
-		foo.connect(foo.getNode(1), foo.getNode(2), 90, 270, 1, null);
-		
-		System.out.println(foo.getNode(1).connectedTo(foo.getNode(2)));
-		System.out.println(foo.getNode(1).getAngle(foo.getNode(0)));
-		System.out.println(foo.getNode(1).getAngle(foo.getNode(2)));
-		System.out.println(foo.edges.size());
-		
-		// Should return 90
-		System.out.println(foo.getNode(1).getAngle(foo.getNode(0), foo.getNode(2)));
+		foo.connect(foo.getNode(1), foo.getNode(2), 90, 270, 1, bar);
 	}
-
 }
