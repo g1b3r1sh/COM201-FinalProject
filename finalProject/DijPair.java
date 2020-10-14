d40a733c697440b5f03e8a92f5a71ee9a63bdff2
repl.it user <replituser@example.com>
2020-10-14 20:59:02 +0000

    Added helper classes for Dij algorithm

---
+++ finalProject/DijPair.java
@@ -0,0 +1,11 @@
+package finalProject;
+
+public class DijPair {
+	public DirEdge edge;
+	public double weight;
+
+	public DijPair(DirEdge edge, double weight) {
+		this.edge = edge;
+		this.weight = weight;
+	}
+}
---
+++ finalProject/DirEdge.java
@@ -0,0 +1,11 @@
+package finalProject;
+
+public class DirEdge {
+	public Node from;
+	public Node to;
+	
+	public DirEdge(Node from, Node to) {
+		this.from = from;
+		this.to = to;
+	}
+}
