bf05fd2baef9e917c1b8fb4abb89213e693cbbdb
repl.it user <replituser@example.com>
2020-10-16 16:11:47 +0000

    Moved dij code into own class

---
+++ finalProject/DijPath.java
@@ -0,0 +1,132 @@
+package finalProject;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class DijPath {
+	Graph g;
+
+	public DijPath(Graph g) {
+		this.g = g;
+	}
+	
+	// returns null if origin or dest don't exist in graph
+	public ArrayList<Node> findPath(int origin, int dest)
+	{
+		Edge originEdge = this.g.getEdge(origin);
+		// Edge cases
+		if (originEdge == null)
+		{
+			return null;
+		}
+		if (originEdge.hasRoom(dest))
+		{
+			return new ArrayList<>();
+		}
+		
+		// Create queue of DijPair that sorts ascending by weight
+		PriorityQueue<DijPair> q = new PriorityQueue<>(10, new Comparator<DijPair>() {
+				public int compare(DijPair p1, DijPair p2)
+				{
+					// Sort from least weight to greatest weight
+					if (p1.weight < p2.weight) return -1;
+					if (p2.weight < p1.weight) return 1;
+					return 0;
+				}
+		});
+		
+		// Represents origin by creating new Node with name of origin - a bit hacky
+		Node originNode = new Node(origin);
+		Node destNode = new Node(dest);
+		// Assert name convension for Nodes and rooms are different
+		assert this.g.getNode(origin) != null : "Origin is name of node in graph";
+		assert this.g.getNode(dest) != null : "Dest is name of node in graph";
+		// Q starts with connection from origin to surrounding nodes
+		q.add(new DijPair(new DirEdge(originNode, originEdge.getNode1()), originEdge.roomDist(origin, originEdge.getNode1())));
+		q.add(new DijPair(new DirEdge(originNode, originEdge.getNode2()), originEdge.roomDist(origin, originEdge.getNode2())));
+		// Basic directed graph of shortest path from origin to Nodes in graph (not every)
+		// Also serves as storage for Nodes already visited by q, since each node can only be pointed at once
+		ArrayList<DirEdge> visited = new ArrayList<>();
+		
+		while (q.size() > 0)
+		{
+			// Pop front of q
+			DijPair currPair = q.poll();
+			// Current node is node that connection is pointing to
+			Node currNode = currPair.edge.to;
+			// Check that node not already visited
+			if (visitedContains(visited, currNode))
+			{
+				continue;
+			}
+			// Add connection to visited graph to prevent Node from being revisited
+			visited.add(currPair.edge);
+			// Check if current node is dest
+			if (currNode == destNode)
+			{
+				return makePath(visited, originNode, destNode);
+			}
+			
+			// Iterates through each Edge that current Node is connected to
+			for (final NodeConnection connection : currNode.getConnections())
+			{
+				// To save time, gets internal representation of Edges in Node and converts to Edge
+				Edge e = connection.getEdge();
+				// Adds new connections into q
+				if (e.hasRoom(dest))
+				{
+					// If edge contains dest, adds connection of current Node to dest
+					q.add(new DijPair(new DirEdge(currNode, new Node(dest)), currPair.weight + e.roomDist(dest, currNode)));
+				}
+				else
+				{
+					// Otherwise, add connection from current Node to Node on other side of Edge
+					q.add(new DijPair(new DirEdge(currNode, e.getOther(currNode)), currPair.weight + e.getWeight()));
+				}
+			}
+		}
+		// If q is empty, there is no path from origin to dest
+		return null;
+	}
+	
+	// Helper functions
+	private static boolean visitedContains(ArrayList<DirEdge> visited, Node n)
+	{
+		for (DirEdge d : visited)
+		{
+			if (d.to == n)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private static Node getFromNode(ArrayList<DirEdge> dirGraph, Node to)
+	{
+		for (DirEdge e : dirGraph)
+		{
+			if (e.to == to)
+			{
+				return e.from;
+			}
+		}
+		return null;
+	}
+	
+	private static ArrayList<Node> makePath(ArrayList<DirEdge> dirGraph, Node origin, Node dest)
+	{
+		ArrayList<Node> path = new ArrayList<>();
+		Node currNode = getFromNode(dirGraph, dest);
+		while (currNode != origin)
+		{
+			path.add(currNode);
+			currNode = getFromNode(dirGraph, currNode);
+		}
+		Collections.reverse(path);
+		return path;
+	}
+
+}
--- finalProject/Program.java
+++ finalProject/Program.java
@@ -25,7 +25,7 @@ public class Program {
 		g.connect(g.getNode(0), g.getNode(1), 180, 0, 2, ZO);
 		g.connect(g.getNode(1), g.getNode(2), 90, 270, 1, OT);
         g.connect(g.getNode(0), g.getNode(2), 135, 315, 1, ZT);
-		ArrayList<Node> path = findPath(103, 111, g);
+		ArrayList<Node> path = new DijPath(g).findPath(103, 111);
 		if (path.size() == 0)
 		{
 			System.out.println("No path");
@@ -34,114 +34,5 @@ public class Program {
 		{
 			System.out.println(n.getName());
 		}
-	}
-	
-	// TODO split into methods
-	// returns null if origin or dest don't exist in graph
-	private static ArrayList<Node> findPath(int origin, int dest, Graph g)
-	{
-		Edge originEdge = g.getEdge(origin);
-		if (originEdge == null)
-		{
-			return null;
-		}
-		if (originEdge.hasRoom(dest))
-		{
-			return new ArrayList<>();
-		}
-		PriorityQueue<DijPair> q = new PriorityQueue<>(10, new Comparator<DijPair>() {
-				public int compare(DijPair p1, DijPair p2)
-				{
-					// Sort from least weight to greatest weight
-					if (p1.weight < p2.weight) return -1;
-					if (p2.weight < p1.weight) return 1;
-					return 0;
-				}
-		});
-		
-		// TODO make use of these variables
-		Node originNode = new Node(origin);
-		Node destNode = new Node(dest);
-		// Assert name convension for Nodes and rooms are different
-		assert g.getNode(origin) != null : "Origin is name of node in graph";
-		assert g.getNode(dest) != null : "Dest is name of node in graph";
-		// Represents origin by creating new Node with name of origin - a bit hacky
-		q.add(new DijPair(new DirEdge(originNode, originEdge.getNode1()), originEdge.roomDist(origin, originEdge.getNode1())));
-		q.add(new DijPair(new DirEdge(originNode, originEdge.getNode2()), originEdge.roomDist(origin, originEdge.getNode2())));
-		ArrayList<DirEdge> visited = new ArrayList<>();
-		
-		while (q.size() > 0)
-		{
-			DijPair currPair = q.poll();
-			Node currNode = currPair.edge.to;
-			if (visitedContains(visited, currNode))
-			{
-				continue;
-			}
-			visited.add(currPair.edge);
-			if (currNode == destNode)
-			{
-				ArrayList<Node> path = new ArrayList<>();
-				currNode = getFromNode(visited, currNode);
-				while (currNode != originNode)
-				{
-					path.add(currNode);
-					currNode = getFromNode(visited, currNode);
-				}
-				Collections.reverse(path);
-				return path;
-			}
-			for (NodeConnection connection : currNode.getConnections())
-			{
-				Edge e = connection.getEdge();
-				if (e.hasRoom(dest))
-				{
-					q.add(new DijPair(new DirEdge(currNode, new Node(dest)), currPair.weight + e.roomDist(dest, currNode)));
-				}
-				else
-				{
-					q.add(new DijPair(new DirEdge(currNode, e.getOther(currNode)), currPair.weight + e.getWeight()));
-				}
-			}
-		}
-		// There is no path from origin to dest
-		return null;
-	}
-	
-	private static boolean visitedContains(ArrayList<DirEdge> visited, Node n)
-	{
-		for (DirEdge d : visited)
-		{
-			if (d.to == n)
-			{
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	private static Node getFromNode(ArrayList<DirEdge> dirGraph, Node to)
-	{
-		for (DirEdge e : dirGraph)
-		{
-			if (e.to == to)
-			{
-				return e.from;
-			}
-		}
-		return null;
-	}
-    
-	private static ArrayList<Node> makePath(ArrayList<DirEdge> dirGraph, Node origin, Node dest)
-	{
-		ArrayList<Node> path = new ArrayList<>();
-		Node currNode = getFromNode(dirGraph, dest);
-		while (currNode != origin)
-		{
-			path.add(currNode);
-			currNode = getFromNode(dirGraph, currNode);
-		}
-		Collections.reverse(path);
-		return path;
 	}
 }
