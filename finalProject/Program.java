eb0a4765206d55bf28f52870cab24cfce7e3f564
repl.it user <replituser@example.com>
2020-10-19 17:39:27 +0000

    Added interface to program

--- finalProject/Program.java
+++ finalProject/Program.java
@@ -1,5 +1,7 @@
 package finalProject;
 
+import java.io.*;
+import java.util.*;
 public class Program {
 	public static void main(String[] args) throws Exception{
 		/*
@@ -13,14 +15,41 @@ public class Program {
 		// There is probably another way to do this using FileLocator or something
 		XMLGraph xml = new XMLGraph("finalProject/data/example_graph.xml");
 		Graph g = xml.constructGraph();
-		Path p = new DijPath(g).findPath("100", "107");
-		if (p.getSize() == 0)
+		Scanner kbReader = new Scanner(System.in);
+		
+		while(true)
 		{
-			System.out.println("No path");
+			System.out.print("Enter origin: ");
+			String origin = kbReader.next();
+			if (g.getEdge(origin) == null)
+			{
+				System.out.println("Origin was not a room.");
+			}
+			
+			System.out.print("Enter destination: ");
+			String dest = kbReader.next();
+			if (g.getEdge(dest) == null)
+			{
+				System.out.println("Destination was not a room.");
+			}
+			Path p = new DijPath(g).findPath(origin, dest);
+			if (p == null)
+			{
+				System.out.println("No path.");
+			}
+			else if (p.getSize() == 0)
+			{
+				System.out.println("Same hallway.");
+			}
+			else
+			{
+				for (Node n : p.getNodes())
+				{
+					System.out.println(n.getName());
+				}
+			}
 		}
-		for (Node n : p.getNodes())
-		{
-			System.out.println(n.getName());
-		}
+
+		kbReader.close();
 	}
 }
